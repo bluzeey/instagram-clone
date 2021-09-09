1634dfe00f83f6d56603e8b2930ff6a56df8d6b2
Sahil Maheshwari <sahilm1711@gmail.com>
2021-09-10 00:50:12 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,9 @@ export default function Header() {
                 <div className="flex justify-between h-full">
                     <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
                         <h1>
-                            <Link to={ROUTES.DASHBOARD} aria-label=""></Link>
+                            <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
+                                <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12" />
+                            </Link>
                         </h1>
                     </div>
                 </div>
